6db75672c0ae5d24c1e17c2398602586e38d03b7
aka-cronos <jorge.escamilla89@gmail.com>
2018-09-14 04:19:30 -0500

    Add basic components and layout

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import registerServiceWorker, { unregister } from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+import registerServiceWorker, { unregister } from './registerServiceWorker'
 import rootReducer from './reducers'
-import App from './App';
+import App from './App'
 
-const versionTracker = require('./versionTracker.json');
+const versionTracker = require('./versionTracker.json')
+
 const store = createStore(rootReducer)
+
+const httpLink = createHttpLink({
+  uri: 'https://heineken-api.valiot.app/api/'
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('auth-token')
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </Router>
   </Provider>,
   document.getElementById('root')
-);
+)
 
 if (versionTracker.version !== localStorage.getItem('version')) {
-  unregister();
-  localStorage.setItem('version', versionTracker.version);
+  unregister()
+  localStorage.setItem('version', versionTracker.version)
 } else {
-  registerServiceWorker();
+  registerServiceWorker()
 }
